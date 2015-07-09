8adfed770db65c472829926e059624b2990ba7f4
Ritual <ritual@dan@unbo.lt>
2015-07-09 10:33:09 +0100

    Adding Entrust

--- config/app.php
+++ config/app.php
@@ -143,7 +143,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+	Zizaco\Entrust\EntrustServiceProvider::class,
     ],
 
     /*
@@ -191,7 +191,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-
+	'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
     ],
 
 ];
