4f225133efa0861912efb9e4f1c10e9bc58b62e0
unbolt <dan@unbo.lt>
2015-07-14 10:18:24 +0100

    Adding Toastr notifications nad updating jquery to v2.1.3

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,6 +1,25 @@
-
-<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
 <link rel="stylesheet" href="{{ elixir("css/app.css") }}">
+
+{!! Toastr::render() !!}
+
+@if (count($errors) > 0)
+    <script>
+        $(document).ready(function() {
+
+            toastr.options.showEasing = 'swing';
+            toastr.options.hideEasing = 'linear';
+            toastr.options.progressBar = true;
+            toastr.options.closeButton = true;
+
+            @foreach ($errors->all() as $error)
+                toastr.error('{{ $error }}');
+            @endforeach
+        });
+    </script>
+@endif
