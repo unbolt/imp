7e70d307f854ebb959344514950ff6689c24875d
unbolt <dan@unbo.lt>
2015-07-14 08:44:55 +0100

    Adding templates, bootstrap, jquery

---
+++ resources/views/partials/scripts.blade.php
@@ -0,0 +1,6 @@
+
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="{{ elixir("css/app.css") }}">
