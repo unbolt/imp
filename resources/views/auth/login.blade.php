57a5eb1a8854d451e6c025e38b986740eab9b034
unbolt <dan@unbo.lt>
2015-07-14 10:18:36 +0100

    Styling the registration form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,39 @@
-<form method="POST" action="/login">
-    {!! csrf_field() !!}
+@extends('layouts.master')
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
+@section('title', 'Login')
+@section('body_class', 'blurbg')
 
-    <div>
-        Password
-        <input type="password" name="password" id="password">
-    </div>
+@section('content')
 
-    <div>
-        <input type="checkbox" name="remember"> Remember Me
+<div class="container login">
+    <div class="row col-md-4 col-md-offset-4">
+        <div class="logo_full"></div>
     </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <form method="POST" action="/login">
+                {!! csrf_field() !!}
+                <div>
+                    <input type="email" name="email" value="{{ old('email') }}" placeholder="Email" class="form-control" tabindex="1" autocomplete="off" autocorrect="off" spellcheck="false">
+                </div>
 
-    <div>
-        <button type="submit">Login</button>
+                <div>
+                    <input type="password" name="password" id="password" placeholder="Password" class="form-control" tabindex="2" autocomplete="off" autocorrect="off" spellcheck="false">
+                </div>
+
+                <div>
+                    <input type="checkbox" name="remember"> Keep me logged in
+                </div>
+
+                <div>
+                    <button id="submit" type="submit" class="btn btn-primary btn-lg btn-block">Log In</button>
+                </div>
+                <div>
+                    <a href="/register" class="btn btn-block btn-large">Register Account</a>
+                </div>
+            </form>
+        </div>
     </div>
-</form>
+</div>
+
+@stop
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,27 +1,34 @@
-<form method="POST" action="/register">
-    {!! csrf_field() !!}
+@extends('layouts.master')
 
-    <div class="col-md-6">
-        Name
-        <input type="text" name="name" value="{{ old('name') }}">
-    </div>
+@section('title', 'Register Account')
+@section('body_class', 'blurbg')
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
+@section('content')
 
-    <div>
-        Password
-        <input type="password" name="password">
+<div class="container register">
+    <div class="row col-md-4 col-md-offset-4">
+        <div class="logo_full"></div>
     </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <form method="POST" action="/register">
+                {!! csrf_field() !!}
 
-    <div class="col-md-6">
-        Confirm Password
-        <input type="password" name="password_confirmation">
-    </div>
+                    <input type="text" name="name" value="{{ old('name') }}" placeholder="Username" class="form-control" tabindex="1" autocomplete="off" autocorrect="off" spellcheck="false">
 
-    <div>
-        <button type="submit">Register</button>
+                    <input type="email" name="email" value="{{ old('email') }}" placeholder="Email" class="form-control" tabindex="1" autocomplete="off" autocorrect="off" spellcheck="false">
+
+                    <input type="password" name="password" id="password" placeholder="Password" class="form-control" tabindex="2" autocomplete="off" autocorrect="off" spellcheck="false">
+
+                    <input type="password" name="password_confirmation" id="password" placeholder="Confirm Password" class="form-control" tabindex="2" autocomplete="off" autocorrect="off" spellcheck="false">
+
+
+                    <div>
+                    <button id="submit" type="submit" class="btn btn-primary btn-lg btn-block">Register</button>
+                </div>
+            </form>
+        </div>
     </div>
-</form>
+</div>
+
+@stop
