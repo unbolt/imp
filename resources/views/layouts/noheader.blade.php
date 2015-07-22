8bbbcbbab7a37fbadf01ce3ac635b95e5681e49c
unbolt <dan@unbo.lt>
2015-07-22 12:14:13 +0100

    Adding UTF8 charset

--- resources/views/layouts/noheader.blade.php
+++ resources/views/layouts/noheader.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-
+        <meta charset="utf-8" />
         <title>@yield('title') - Implausible Conclave - Free Company - Moogle</title>
 
         @include('partials.scripts')
